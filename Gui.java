b430921caa2d929bfe2ec73a052d5abc476c9e41
Cvalladares4837 <Cvalla4@uic.edu>
2015-11-05 11:03:49 -0600

    A Preliminary Gui
    Buttons and News Feed

--- Gui.java
+++ Gui.java
@@ -1,57 +1,164 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.*;
 
-//New changes comments
-
-public class Gui extends JFrame
+public class GUI extends JApplet implements ActionListener, ItemListener
 {
-    JPanel jp = new JPanel();
-    JButton but = new JButton("Hi");
-    JMenuBar bar ;
-    JMenu file;
-    JMenuItem newItem ;
-    JMenuItem exit ;
+
+    //Layouts
+    BorderLayout base;                // The base layout for all the GUI
     
-    public Gui()
+    //Panels
+    private JPanel top;               // top is the top panel of the GUI and contains
+                                      //    drawings
+    private JPanel bottom;            // bottom is the bottom panel of the GUI and 
+                                      //    contains controls
+    
+    private JPanel bottomLeft;        // bottomLeft contains the buttons
+    private JPanel bottomRight;       // bottomRight contains the newsFeed section
+    
+    
+    //Widgets
+    //  Left Side
+    private JButton insertToHeap;     // insertToHeap will let the user insert 
+                                      //    insert to the heap
+    private JButton searchValue;      // searchValue button will let the user search
+                                      //    a value
+    private JButton deleteValue;      // deleteValue button will promt the user to 
+                                      //    delete a value (min or max depending on
+                                      //    heap.
+    private JTextField valueToInsert; // valueToInsert will hold the text value to be inserted
+                                      
+    private JTextField valueToSearch; // valueToSearch will hold the value to be searched
+    
+    
+    //  Right Side
+    private JTextArea feedContents;       // feedContents holds content for news feed.       
+    private JScrollPane feedPane;         // feedPane holds feedContents for news feed.    
+    private String feedMessage;           // message for news feed.        
+    
+   
+    public void init()
+    // POST: Initialize the GUI
     {
-        
-        
-        this.setTitle("Min/Max Heap Tutorial");
-        this.setSize(1500, 1500);
-        this.setResizable(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        
-        bar = new JMenuBar();
-        this.setJMenuBar(bar);
-        
-        file = new JMenu("File");
-        bar.add(file);
-        
-        newItem= new JMenuItem("New");
-        exit = new JMenuItem("Exit");
-        file.add(newItem);
-        file.addSeparator();
-        file.add(exit);
+
 
         
         
-        
+        initializePanels();
+        initializeWidgets();
+        setUpPanels();
+        addToPanels();
+        addToGui();
         
     }
     
     public void paint(Graphics g)
     {
+        super.paint(g);
+    }
+    
+    public void addToGui()
+    // POST: Will add all widgets, layouts, and panels to the GUI
+    {
+        setLayout(base);   //Will set a borderLayout layout as the base 
+                           //      Layout     
+        
+        add(base.NORTH, top);
+        add(base.SOUTH, bottom);        
+    }
+    
+    public void addToPanels()
+    // POST: Will add all widgets to their respective panels
+    {        
+        // Insert Bottom Left Widgets
+        bottomLeft.add(insertToHeap);
+        bottomLeft.add(valueToInsert);
+        bottomLeft.add(searchValue);
+        bottomLeft.add(valueToSearch);
+        bottomLeft.add(deleteValue);
+        
+        // Insert the News Feed
+        bottomRight.add(feedPane);
+        
         
     }
     
-
-    public static void main(String args[])
+    public void setUpPanels()
+    // POST: Will initialize the panel Hierarchy to before adding buttons and
+    //       elements.   It will also set proportions and Initial Sizes for 
+    //       these panels
     {
-        Gui g = new Gui();
+        
+        top.setMinimumSize(new Dimension(600,400 - 130));//Will set the minimum size
+                                                         //  for the TOP panel to 
+                                                         //  2/3 the required project t
+                                                         //  heighT and a full width
+
+        bottom.setMinimumSize(new Dimension(600,130));   //Will set the minimum size
+                                                         //  for the bottom panel to 
+                                                         //  1/3 the required project 
+                                                         //  height and a full width
+        
+        
+        
+        bottom.add(bottomLeft);
+        bottom.add(bottomRight);       
     }
+    
+    public void initializeWidgets()                                         
+    // POST: Will initialize all the widgets in the applet                  
+    {         
+        
+        //Left Side                                                         
+        insertToHeap = new JButton("Insert To Heap");          
+        searchValue = new JButton ("Search Value");
+        deleteValue = new JButton("Delete Value");
+        
+        valueToInsert = new JTextField (); 
+        valueToSearch = new JTextField (); 
+        
+        //Right Side
+        feedContents = new JTextArea();                                  
+        feedContents.setEditable(false);                    
+        feedContents.setText(" ");                          
+        
+        feedPane = new JScrollPane(feedContents);
+    }                                                                       
+    
+    public void initializePanels()                        
+    //POST:  Will initialize all Panels and Layouts      
+    {             
+        base = new BorderLayout();        
+        
+        //Set up all panels                               
+        top = new JPanel();                             
+        bottom = new JPanel();                             
+        bottomRight  = new JPanel();                              
+        bottomLeft = new JPanel();   
+        
+        //Set the layout for all panels
+        top.setLayout(new FlowLayout());
+        bottom.setLayout(new GridLayout(1,2));
+        bottomLeft.setLayout(new GridLayout(5,1));
+        bottomRight.setLayout(new BorderLayout()); 
+        bottomLeft.setMinimumSize(new Dimension(600,130));
+    }                                                     
+    
+    @Override
+    public void itemStateChanged(ItemEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
