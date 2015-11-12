27b03631d43ae3aab90da90d464de5dee4f8d6ce
Fayaz Khan <fkhan42@uic.edu>
2015-11-12 12:04:30 -0600

    Still needs work

--- ScaledPoint.java
+++ ScaledPoint.java
@@ -1,7 +1,9 @@
 // Programmer:  Team 16: Christian Valladares, Joe Kallarackel, Fayaz Khan, Jaydeep Patel
 // Assignment:  Project 3 Heap Illustration 
 // Date:        November 12, 2015
-// Description: A class that properly resizes all the objects. This class also extends JApplet to draw. 
+// Description: A class that resizes all the objects.This class also extends JApplet to draw. 
+
+
 import javax.swing.*;
 
 public class ScaledPoint extends JApplet
@@ -10,29 +12,31 @@ public class ScaledPoint extends JApplet
     int treeHeight;     // Counts the height of the tree, including the root.
     
     public ScaledPoint(int eltNumber)
+    // PRE:  eltNumber is initialized, eltNumber > 0 
     // POST: Creates an instance of Scaled Point and initializes the member variable:
     //       numElements = eltNumber
     {
-        double logElements; //To hold the log_2 of the number of elements
+        double logElements; 								//For log_2 of the number of elements
         
         numElements = eltNumber;    
-        //Will use the property of logarithms log_2 (a) = log_e(x)/log_2(e)to find
-        //  the height of the tree, which happens to be lg_2(numberOfElements)
-        logElements = (Math.log(numElements)/ Math.log(2));
-        treeHeight = (int) (logElements) + 1;         //Get the ceiling of the logarithm
+        
+    
+        logElements = (Math.log(numElements)/ Math.log(2));	//formula for calc height of tree
+        
+        treeHeight = (int) (logElements) + 1;         		//Get the ceiling of the logarithm
     }
     
     public ScaledPoint()                                                
     // POST: Creates an instance of Scaled Point and initializes the member variable:
-    //       numElements = 32
+    //       numElements = 63
     {
-        this(32);
+        this(63);
     }
     
     public int getEdgeXStartPosition(JPanel target, int elementIndex, int nodeWidth)
-    // PRE: target -- must be the panel that holds the tree
-    //      elementIndex -- the Index of the current Index.  Must be greater than 1
-    //      nodeWidth -- the width of the nodes for the drawing
+    // PRE: target is initialized 
+    //      elementIndex is initialized and elementIndex > 1.
+    //      nodeWidth is initialized and nodeWidth > 0. 
     // POST: will return the start value for a node's edge.  We intend to draw from
     //          child to parent.
     //       FCTVAL =
@@ -43,12 +47,12 @@ public class ScaledPoint extends JApplet
     }
     
     public int getEdgeXEndPosition(JPanel target, int elementIndex, int nodeWidth)
-    // PRE:  target -- must be the panel that holds the tree
-    //       elementIndex -- the Index of the current Index.  Must be greater than 0
-    //       nodeWidth -- the width of the nodes for the drawing
-    // POST: will return the start value for a node's edge.  We intend to draw from
+    // PRE:  target is initialized 
+    //       elementIndex is initialized and elementIndex > 1.
+    //       nodeWidth is initialized and nodeWidth > 0. 
+    // POST: will return the start value for a node's edge.We intend to draw from
     //       child to parent.
-    //       FCTVAL =
+    //       FCTVAL = will return x-cordinate for node position 
     //
     {        
         int parent;
@@ -62,25 +66,26 @@ public class ScaledPoint extends JApplet
     }
     
     public int getEdgeYStartPosition(JPanel target, int elementIndex, int nodeWidth)
-    // PRE:  target -- must be the panel that holds the tree
-    //       elementIndex -- the Index of the current Index.  Must be greater than 1
-    //       nodeWidth -- the width of the nodes for the drawing
+    // PRE:  target is initialized 
+    //       elementIndex is initialized and elementIndex > 1.
+    //       nodeWidth is initialized and nodeWidth > 0. 
     // POST: will return the start value for a node's edge.  We intend to draw from
     //       child to parent.
-    //       FCTVAL =
+    //       FCTVAL ==  will return y-cordinate for node position 
     //
     {        
         int yStart = getNodeYPosition(target,elementIndex);
+        
         return yStart;
     }
     
     public int getEdgeYEndPosition(JPanel target, int elementIndex, int nodeWidth)
-    // PRE:  target -- must be the panel that holds the tree
-    //       elementIndex -- the Index of the current Index.  Must be greater than 0
-    //       nodeWidth -- the width of the nodes for the drawing
-    // POST: will return the start value for a node's edge.  We intend to draw from
+    // PRE:  target is initialized 
+    //       elementIndex is initialized and elementIndex > 1.
+    //       nodeWidth is initialized and nodeWidth > 0. 
+    // POST: will return the end value for a node's edge.We intend to draw from
     //       child to parent.
-    //       FCTVAL =
+    //       FCTVAL == will return x-cordinate for node position 
     //
     {        
         int parent;
@@ -94,9 +99,11 @@ public class ScaledPoint extends JApplet
     }
     
     public int getNodeXPosition(JPanel target, int elementIndex)
+    // PRE:  target is initialized 
+    //       elementIndex is initialized and elementIndex > 1.
     // POST: Returns the Horizontal origin for a node in the heap. based on the 
     //       element number, and the total number of elements
-    //       FCTVAL = Width *(elementIndex / (2^treeHeight)
+    //       FCTVAL == Width *(elementIndex / (2^treeHeight)
     {
         int width;      // width holds the width of the panel
         int position;   // position holds the pixel position of a given node element.                
@@ -109,26 +116,30 @@ public class ScaledPoint extends JApplet
                         //    odd number
  
         width = target.getWidth();                                    // Fetch the pane Width
-        nodeLevel = (int) (Math.log(elementIndex + 1)/ Math.log(2));  // Compute the node level log_2(elementIndex) 
+        nodeLevel = (int) (Math.log(elementIndex + 1)/ Math.log(2));  // level log_2(elementIndex) 
                                                                       // (to avoid zero values)
     
         iterator = ((elementIndex+1) % (int) Math.pow(2, nodeLevel)); // Compute the node's index for its level, 
-                                                                      //    which is the element's index modded with
+                                                                      //    which is the element's index mod with
                                                                       //    the total number of elements for a 
                                                                       //    given level.
+        
         numerator = (iterator)*2 + 1;                                 // The numerator is the node's level-index, 
                                                                       //    turned to an odd number
         position = (int) (width  * numerator / 
                    (Math.pow(2.0,(double) nodeLevel + 1)));           // Get the position as a percentage of the panel
                                                                       //    width. This will be the element number
                                                                       //    divided by 2 to the Tree Height.
-       return position;                                               // return the positon 
+        
+       return position;                                               // return the position 
     }
     
     public int getNodeYPosition(JPanel target, int elementIndex)
+    // PRE:  target is initialized 
+    //       elementIndex is initialized and elementIndex > 1.
     // POST: Returns the Vertical origin for a node in the heap. based on the 
     //       element number, and the total number of elements
-    //       FCTVAL = Width *(elementIndex / (2^treeHeight)
+    //       FCTVAL == Width *(elementIndex / (2^treeHeight)
     {
         int height;     // Holds the height of the panel
         int position;   // Holds the pixel position of a given node element.                
